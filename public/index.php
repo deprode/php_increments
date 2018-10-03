4b96ad00e1de177304309d7242892cae84f45c60
deprode <sensitively+github@gmail.com>
2018-10-03 23:53:51 +0900

    エラー表示設定をビルトインサーバの時だけに修正
    * ビルトインサーバの時だけエラー関連の表示設定を行うように修正した
    * display_errorsは普通に無効でいいと思うが、表示されたらまずいエラーがあるので開発用のサーバだけにして、後はphp.iniなどで設定したほうがよい
    * でも、エラーハンドラは設定してmonologでログ収集したりしてもいいかな、と思った

--- public/index.php
+++ public/index.php
@@ -4,13 +4,24 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 session_start();
 
-// TODO: エラー関連
-ini_set("display_errors", 0);
-ini_set("display_startup_errors", 0);
-error_reporting(E_ALL);
-set_error_handler(function ($errno, $errstr, $errfile, $errline) {
-    throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
-}, E_ALL ^ E_DEPRECATED ^ E_USER_DEPRECATED ^ E_USER_NOTICE);
+// エラー関連の設定は開発時かつcliの時だけ
+if (PHP_SAPI === 'cli-server') {
+    ini_set("display_errors", 0);
+    ini_set("display_startup_errors", 0);
+    error_reporting(E_ALL);
+
+    set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+        throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
+    }, E_ALL ^ E_DEPRECATED ^ E_USER_DEPRECATED ^ E_USER_NOTICE);
+
+    set_exception_handler(function ($e) {
+        $fp = fopen('php://stderr', 'w');
+        fputs($fp, $e);
+        header('Content-Type: text/plain; charset=utf-8', true, 500);
+        echo 'エラーです';
+        exit(1);
+    });
+}
 
 require_once __DIR__ . '/../src/dependencies.php';
 
