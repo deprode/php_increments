d86fa50de8d32100eb430d953fed64f1d17bf669
deprode <sensitively+github@gmail.com>
2018-10-06 15:07:44 +0900

    ミドルウェアのテストを追加した
    * SecurityHeaderMiddlewareのテストを追加した
    * テストが簡単に書ける代わりに、Dispatcherのようにリクエストとリクエストハンドラーを作ってdispatchする機構をMiddlewares\Utils\Dispatcherにテスト時も依存しており、どうなのかな…と思わないでもない。

---
+++ tests/Middleware/SecurityHeaderMiddlewareTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Tests;
+
+use Middlewares\Utils\Dispatcher;
+use App\Middleware\SecurityHeaderMiddleware;
+use PHPUnit\Framework\TestCase;
+
+class SecurityHeaderMiddlewareTest extends TestCase
+{
+    public function testProcess()
+    {
+        $response = Dispatcher::run([
+            new SecurityHeaderMiddleware()
+        ]);
+
+        $this->assertSame('SAMEORIGIN', $response->getHeader('X-Frame-Options')[0]);
+        $this->assertSame('1; mode=block', $response->getHeader('X-XSS-Protection')[0]);
+        $this->assertSame('nosniff', $response->getHeader('X-Content-Type-Options')[0]);
+    }
+}
